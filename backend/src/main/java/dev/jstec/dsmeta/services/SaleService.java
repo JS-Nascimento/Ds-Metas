851864ebc1fce6ff1add6a08eb223ab86c59e92c
JS-Nascimento <jorge@jstec.dev.br>
2022-11-16 19:52:10 -0300

    Date consulting

--- backend/src/main/java/dev/jstec/dsmeta/services/SaleService.java
+++ backend/src/main/java/dev/jstec/dsmeta/services/SaleService.java
@@ -1,9 +1,13 @@
 package dev.jstec.dsmeta.services;
 
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.jstec.dsmeta.entities.Sale;
@@ -15,9 +19,15 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+				
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = maxDate.isEmpty() ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.isEmpty() ? today :  LocalDate.parse(maxDate);
 
-		return repository.findAll();
+		return repository.findSales(min, max, pageable);
 		
 	}
 
