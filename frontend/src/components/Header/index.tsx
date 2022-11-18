c279b1f3022b0918d8e56d4fb08ff955f11f303d
JS-Nascimento <jorge@jstec.dev.br>
2022-11-18 18:47:57 -0300

    deploy version

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="" />
-                <h1>DS Metas</h1>
+                <h1>JS Metas</h1>
                 <p>Desenvolvido por Jorge Nascimento</p>
 
             </div>
